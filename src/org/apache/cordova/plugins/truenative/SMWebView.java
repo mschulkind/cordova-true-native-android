38d091863720abf2aab51066e8171c29da52aeda
Matthew Schulkind <mschulkind@gmail.com>
2012-04-14 19:36:29 -0400

    Everything with spidermonkey seems to be working. Example app now runs.

--- src/org/apache/cordova/plugins/truenative/SMWebView.java
+++ src/org/apache/cordova/plugins/truenative/SMWebView.java
@@ -3,6 +3,10 @@ package org.apache.cordova.plugins.truenative;
 import android.webkit.WebView;
 import org.apache.cordova.DroidGap;
 
+import java.util.concurrent.locks.*;
+
+import static junit.framework.Assert.*;
+
 public class SMWebView extends WebView {
   private SMRuntime mSMRuntime;
   private DroidGap mDroidGap;
@@ -13,12 +17,46 @@ public class SMWebView extends WebView {
     mSMRuntime = new SMRuntime(droidGap, sourceFiles);
 
     writeJavascript(
-        "require('cordova/channel').onNativeReady.fire(); _nativeReady = true;");
+        "window.onBodyLoad(); "
+        + "require('cordova/channel').onNativeReady.fire();"
+        + "_nativeReady = true;");
+  }
+
+  private class WJRunnable implements Runnable {
+    public String result;
+    private SMRuntime mSMRuntime;
+    private String mCode;
+    final public Lock lock = new ReentrantLock();
+    final public Condition hasResult = lock.newCondition();
+
+    public WJRunnable(SMRuntime smRuntime, String code) {
+      mSMRuntime = smRuntime;
+      mCode = code;
+    }
+
+    public void run() {
+      lock.lock();
+      result = mSMRuntime.writeJavascript(mCode);
+      hasResult.signal();
+      lock.unlock();
+    }
   }
 
   public String writeJavascript(String code) {
-    System.out.println("javascript: "+code);
-    return mSMRuntime.writeJavascript(code);
+    //System.out.println("javascript: "+code);
+
+    WJRunnable runnable = new WJRunnable(mSMRuntime, code);
+    runnable.lock.lock();
+    mDroidGap.runOnUiThread(runnable);
+    try {
+      while (runnable.result == null) runnable.hasResult.await();
+    } catch(InterruptedException e) {
+      e.printStackTrace();
+      fail("failed waiting for a result from writeJavascript");
+    }
+    runnable.lock.unlock();
+
+    return runnable.result;
   }
 
   @Override
